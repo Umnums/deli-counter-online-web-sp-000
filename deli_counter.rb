09bfcd2e2958085c7ba4cf0979b9cd98b1dd78b3
Joseph Kao <josephtkao@gmail.com>
2020-04-07 21:24:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ def line(array)
   if array.length == 0
   puts  "The line is currently empty."
   else
-    string = "The line is currently: "
+    string = "The line is currently:"
     array.each_with_index do |name, index|
-      string << "#{index + 1}. #{name} "
+      string << " #{index + 1}. #{name}"
     end
     puts "#{string}"
   end
