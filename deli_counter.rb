40fedab212bc717dbbecee9ed94789ad22b51afb
Joseph Kao <josephtkao@gmail.com>
2020-04-07 21:31:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,12 @@ def take_a_number(line, person)
   line.push(person)
   puts "Welcome, #{person}. You are number #{line.length} in line."
 end
+
+def now_serving(line)
+  if line.length == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line[0]}."
+    line.shift
+  end
+end
